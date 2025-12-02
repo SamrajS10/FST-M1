66bed4254c107b3866cc0acfd840258d7a576c80
SamrajS10 <samraj.s@ibm.com>
2025-12-02 08:45:28 +0530

    Add files via upload

---
+++ Java/Activities/Car.java
@@ -0,0 +1,35 @@
+package Activity1;
+
+public class Car 
+	{
+    String color;
+    String transmission;
+    int make;
+    int tyres;
+    int doors;
+
+    public Car() 
+    {	
+        this.tyres = 4;
+        this.doors = 4;
+    }
+
+    public void displayCharacteristics() 
+    {
+        System.out.println("Car Color: " + color);
+        System.out.println("Transmission: " + transmission);
+        System.out.println("Make: " + make);
+        System.out.println("Number of Tyres: " + tyres);
+        System.out.println("Number of Doors: " + doors);
+    }
+
+    public void accelarate() 
+    {
+        System.out.println("Car is moving forward.");
+    }
+
+    public void brake() 
+    {
+        System.out.println("Car has stopped.");
+    }
+}
