e813b44de369add016fcab63a050eabfb940737f
SamrajS10 <samraj.s@ibm.com>
2025-12-14 11:58:31 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,21 @@
+package Activity12;
+
+interface Addable 
+{
+    int add(int num1, int num2);
+}
+
+public class Activity12 
+{
+    public static void main(String[] args) 
+    {
+        Addable ad1 = (a, b) -> (a + b);
+        Addable ad2 = (int a, int b) -> 
+        {
+            return (a + b);
+        };
+        System.out.println("Result from ad1: " + ad1.add(5, 10));
+        System.out.println("Result from ad2: " + ad2.add(20, 30));
+    }
+}
+
