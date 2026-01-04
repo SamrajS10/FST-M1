6f66f0600f9440da01ec06a77571e6c470fce5bd
SamrajS10 <samraj.s@ibm.com>
2026-01-04 21:26:56 +0530

    Add files via upload

---
+++ Appium/Activities/Activity2.java
@@ -0,0 +1,52 @@
+package Activity2;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Activity2 
+{
+    AndroidDriver driver;
+    @BeforeClass
+    public void setUp() throws MalformedURLException, URISyntaxException 
+    {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.chrome");
+        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.noReset();
+
+        URL serverURL = new URI("http://localhost:4723").toURL();
+        driver = new AndroidDriver(serverURL, options);
+        driver.get("https://training-support.net");
+    }
+
+    @Test
+    public void chromeTest() 
+    {
+        String pageHeading = driver.findElement(AppiumBy.xpath(
+                "//android.widget.TextView[@text='Training Support']"
+        )).getText();
+        System.out.println("Heading: " + pageHeading);
+        driver.findElement(AppiumBy.accessibilityId("About Us")).click();
+        String aboutPageHeading = driver.findElement(AppiumBy.xpath(
+                "//android.widget.TextView[@text='About Us']"
+        )).getText();
+        System.out.println(aboutPageHeading);
+    }
+
+    @AfterClass
+    public void tearDown() 
+    {
+        driver.quit();
+    }
+}
